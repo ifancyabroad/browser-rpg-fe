941ea42e263c6c262af9052fec0a1fd217778a8b
Edgar Nightingale <edgar.nightingale@btinternet.com>
2023-12-11 10:12:19 +0000

    Update mobile header UI

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, IconButton, Menu, MenuItem, Typography } from "@mui/material";
+import { Box, IconButton, Menu, MenuItem, Typography, keyframes, styled } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import { Menu as MenuIcon } from "@mui/icons-material";
 import { logout } from "features/authentication";
@@ -9,12 +9,43 @@ import PsychologyIcon from "@mui/icons-material/Psychology";
 import { openCharacterSheet } from "features/character";
 import { useMatch } from "react-router-dom";
 
+const pulse = keyframes`
+	0% {
+		transform: scale(0.1);
+		box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+	}
+	
+	70% {
+		transform: scale(1);
+		box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
+	}
+	
+	100% {
+		transform: scale(0.1);
+		box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
+	}
+`;
+
+const Pulse = styled("div")({
+	position: "absolute",
+	height: "100%",
+	width: "100%",
+	left: 0,
+	top: 0,
+	borderRadius: "50%",
+	boxShadow: "0 0 0 0 rgba(255, 255, 255, 1)",
+	animation: `${pulse} 2s infinite`,
+});
+
 export const Header: React.FC = () => {
 	const dispatch = useAppDispatch();
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 	const open = Boolean(anchorEl);
 	const isLoggedIn = useAppSelector((state) => state.authentication.session);
-	const isGame = useMatch("/game");
+	const isGame = useMatch({ path: "/game", end: false });
+	const isBattle = useMatch({ path: "/game/battle", end: true });
+	const isCharacterSheetOpen = useAppSelector((state) => state.character.isCharacterSheetOpen);
+	const showPulse = isBattle && !isCharacterSheetOpen;
 
 	const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorEl(event.currentTarget);
@@ -47,26 +78,11 @@ export const Header: React.FC = () => {
 				borderBottom: "1px solid #000",
 			}}
 		>
-			<IconButton
-				aria-label="menu"
-				color="inherit"
-				type="button"
-				onClick={handleDrawerToggle}
-				hidden={Boolean(isGame)}
-				sx={{ display: { md: "none" }, visibility: !isGame ? "hidden" : null }}
-			>
-				<PsychologyIcon fontSize="small" />
-			</IconButton>
-
 			<Box
 				sx={{
 					flexGrow: 1,
 					display: "flex",
 					alignItems: "center",
-					justifyContent: {
-						xs: "center",
-						md: "flex-start",
-					},
 					gap: 2,
 				}}
 			>
@@ -76,9 +92,26 @@ export const Header: React.FC = () => {
 				</Typography>
 			</Box>
 
-			<IconButton aria-label="menu" color="inherit" type="button" onClick={handleOpenMenu}>
-				<MenuIcon fontSize="small" />
-			</IconButton>
+			<Box>
+				<IconButton
+					aria-label="menu"
+					color="inherit"
+					type="button"
+					onClick={handleDrawerToggle}
+					sx={{
+						display: { md: "none" },
+						position: "relative",
+						visibility: !isGame ? "hidden" : null,
+					}}
+				>
+					<Pulse sx={{ display: showPulse ? "block" : "none" }} />
+					<PsychologyIcon fontSize="small" />
+				</IconButton>
+				<IconButton aria-label="menu" color="inherit" type="button" onClick={handleOpenMenu}>
+					<MenuIcon fontSize="small" />
+				</IconButton>
+			</Box>
+
 			<Menu
 				id="lock-menu"
 				anchorEl={anchorEl}
