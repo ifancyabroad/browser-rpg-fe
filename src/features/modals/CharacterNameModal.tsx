143d7e4764af9a3b335a8afb51fc16f140dd8275
Edgar Nightingale <edgar.nightingale@btinternet.com>
2024-09-22 17:35:32 +0100

    Add name generator

--- src/features/modals/CharacterNameModal.tsx
+++ src/features/modals/CharacterNameModal.tsx
@@ -1,6 +1,7 @@
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Link, TextField } from "@mui/material";
 import { useAppSelector } from "common/hooks";
-import { useRef } from "react";
+import { useState } from "react";
+import { nameByRace } from "fantasy-name-generator";
 
 interface IProps {
 	isOpen: boolean;
@@ -11,14 +12,26 @@ interface IProps {
 export const CharacterNameModal: React.FC<IProps> = ({ isOpen, onClose, onConfirm }) => {
 	const status = useAppSelector((state) => state.character.status);
 	const isLoading = status === "loading";
-	const nameRef = useRef<HTMLInputElement>(null);
+	const generatedName = nameByRace("human");
+	const [name, setName] = useState(typeof generatedName === "string" ? generatedName : "");
 
 	const handleClose = () => {
 		onClose();
 	};
 
 	const handleConfirm = async () => {
-		onConfirm(nameRef.current!.value);
+		onConfirm(name);
+	};
+
+	const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setName(event.target.value);
+	};
+
+	const handleGenerateName = () => {
+		const generatedName = nameByRace("human");
+		if (typeof generatedName === "string") {
+			setName(generatedName);
+		}
 	};
 
 	return (
@@ -27,7 +40,8 @@ export const CharacterNameModal: React.FC<IProps> = ({ isOpen, onClose, onConfir
 			<DialogContent>
 				<DialogContentText mb={2}>Please give your character a name.</DialogContentText>
 				<TextField
-					inputRef={nameRef}
+					onChange={handleChangeName}
+					value={name}
 					autoFocus
 					margin="dense"
 					id="name"
@@ -35,9 +49,16 @@ export const CharacterNameModal: React.FC<IProps> = ({ isOpen, onClose, onConfir
 					type="text"
 					fullWidth
 					required
+					inputProps={{
+						minLength: 3,
+						maxLength: 10,
+					}}
 				/>
 			</DialogContent>
 			<DialogActions>
+				<Link component="button" onClick={handleGenerateName}>
+					Generate Name
+				</Link>
 				<Link component="button" color="text.secondary" onClick={handleClose}>
 					Cancel
 				</Link>
